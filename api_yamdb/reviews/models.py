8b4bb5a97cf0ca92844cf750868af3060de6c04d
solilov <alexandr.solilov@yandex.ru>
2021-08-09 18:44:57 +0300

    Добавлены модели Categoty, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -10,3 +10,51 @@ class User(AbstractUser):
         blank=True,
     )
     role = models.CharField(verbose_name='Роль пользователя', max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Категория')
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Жанр')
+    slug = models.SlugField(unique=True, null=False)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Нзвание произведения')
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Нзвание произведения'
+    )
+    description = models.TextField(verbose_name='Описание')
+    genre = models.ForeignKey(Genre,
+                              on_delete=models.CASCADE,
+                              blank=True,
+                              null=True,
+                              related_name='titles',
+                              verbose_name='Жанр')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 blank=True,
+                                 null=True,
+                                 related_name='titles',
+                                 verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
