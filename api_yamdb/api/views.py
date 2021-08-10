76033f9638de19126a63a9ad893210cffae255a5
solilov <alexandr.solilov@yandex.ru>
2021-08-10 14:05:51 +0300

    Добавил в приложение api файлы: serializers.py и urls.py Сделал вьюесты, сериализаторы для моделей Category и Genre, добавил роутер на них в api.urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from reviews.models import Category, Genre
+from .serializers import CategorySerializer, GenreSerializer
 
-# Create your views here.
+
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    """
+    Вьюесет модели Category
+    CategoryViewSet реализует операции:
+        <POST> - добавление новой категории, достпуно только Администратору
+        <GET> - получение списка категорий, доступно без токена
+        <DELETE> - удаление категории, доступно только Администратору
+    Есть поиск по полю 'name'
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = ()
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class GenreViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    """
+    Вьюесет модели Genre
+    GenreViewSet реализует операции:
+        <POST> - добавление новой категории, достпуно только Администратору
+        <GET> - получение списка категорий, доступно без токена
+        <DELETE> - удаление категории, доступно только Администратору
+    Есть поиск по полю 'name'
+    """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = ()
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
